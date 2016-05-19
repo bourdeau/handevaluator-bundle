e21042d8d059156da72aa2c1d80987059eee9f8d
Pierre-Henri <phbasic@gmail.com>
2016-05-19 09:41:30 +0200

    A bit of behat

---
+++ features/bootstrap/HandEvaluatorContext.php
@@ -0,0 +1,50 @@
+<?php
+
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\Context;
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+
+use Bourdeau\Bundle\HandEvaluatorBundle\HandEvaluator\HandFinder;
+use Bourdeau\Bundle\HandEvaluatorBundle\HandEvaluator\CardValidator;
+
+/**
+ * Defines application features from the specific context.
+ */
+class HandEvaluatorContext implements Context, SnippetAcceptingContext
+{
+    private $handFinder;
+    private $cards;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct()
+    {
+        $this->handFinder = new HandFinder(new CardValidator());
+    }
+
+    /**
+     * @Given I have the following cards:
+     */
+    public function iHaveTheFollowingCards(TableNode $table)
+    {
+        $this->cards = $table->getRow(0);
+    }
+
+    /**
+     * @Then I should get a :arg1 with a rank :arg2
+     */
+    public function iShouldGetAWithARank($arg1, $arg2)
+    {
+        $result = $this->handFinder->findHand($this->cards);
+
+        PHPUnit_Framework_Assert::assertContains($arg1, $result);
+        PHPUnit_Framework_Assert::assertContains($arg2, $result);
+    }
+}
---
+++ features/handevaluator/handevaluator.feature
@@ -0,0 +1,10 @@
+Feature: HandEvaluator
+    I have some poker cards and I want to know if I have a valid poker hand
+    If I have one, I need to know it's name and it's rank. I also need to get
+    the cards that constitue the hand back.
+
+    Scenario: Find a Royal Flush
+        Given I have the following cards:
+            | AS | KS | QS | JS | 10S | 9S | 8S |
+        Then I should get a "Royal Flush" with a rank "13" with the following cards:
+            | AS | KS | QS | JS | 10S |
